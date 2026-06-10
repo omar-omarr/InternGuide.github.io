c57a827c70358046fe800bfff21cf237a836830d
omar-omarr <omarhussienomar555@gmail.com>
2026-06-10 13:26:06 +0300

    Configure production backend API

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
 
-  // Set this to the deployed backend URL, for example: https://api.example.com/api
-  window.INTERNGUIDE_API_BASE = window.INTERNGUIDE_API_BASE || '';
+  window.INTERNGUIDE_API_BASE =
+    window.INTERNGUIDE_API_BASE || 'https://internguide-api-production.up.railway.app/api';
 })();
